989096d756f679805058acd4e85d3323676d3365
Rabeea <rabeeasafadi@gmail.com>
2024-11-08 02:17:16 +0200

    Added dark/light theme toggling
    1. Added new button to switch between themes.
    2. Added presistant theme variable in local storage.
    3. Saved theme gets loaded before react initializes, defaults to light and presists it.

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,23 @@ export function App() {
         SetModalShown(false);
     }
 
+    function onThemeSwitch() {
+        const currentTheme = localStorage.getItem("current-theme") ?? "light";
+        const targetTheme = currentTheme == "light" ? "dark" : "light";
+
+        localStorage.setItem("current-theme", targetTheme);
+        document.querySelector("html")!.setAttribute("data-bs-theme", targetTheme);
+    }
+
     return (
         <main className="container my-5">
             <div className="vstack align-items-center gap-3">
                 <h1 className="fs-2">Hello, World!</h1>
                 <button className="btn btn-success" onClick={onDoSomething}>
                     Do Something
+                </button>
+                <button className="btn btn-secondary" onClick={onThemeSwitch}>
+                    Switch Theme
                 </button>
             </div>
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
 import "./index.css";
 
+const savedTheme = localStorage.getItem("current-theme") ?? "light";
+document.querySelector("html")!.setAttribute("data-bs-theme", savedTheme);
+
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <App />
